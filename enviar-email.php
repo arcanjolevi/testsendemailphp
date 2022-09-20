c8a81ecfbecea898a6e2653fb3a47bcfef9d89e3
Levi Arcanjo <arcanjolevi@gmail.com>
2022-09-20 16:59:14 -0300

    update

--- enviar-email.php
+++ enviar-email.php
@@ -21,12 +21,16 @@
   $assunto = "Contato pelo Site";
 
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+  $headers  = "From:arcanjolevi@gmail.com"."\r\n".
+              "Reply-To:".$email."\r\n".
+              "X-Mailer: PHP/".phpversion();
 
   //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
+  if(mail($destino, $assunto, $arquivo, $headers)){
+    echo("<script>alert('E-mail enviado com sucesso!');</script>");
+  }else{
+    echo("<script>alert('Falha ao enviar o e-mail!');</script>");
+  }
   
-  echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
+  // echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
 ?>
